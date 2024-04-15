b4b3b73ee8844e3f79a2a1bc07aff4030e5a0994
Duncan Bourquard <bourquardduncan@gmail.com>
2024-04-15 17:52:41 +0200

    Adding security manners and add sounds manually to the soundboard

--- kevinmetal.py
+++ kevinmetal.py
@@ -8,35 +8,60 @@ import threading
 from discord.ext import commands
 from os import listdir
 from os.path import isfile, join
-allSounds = [f for f in listdir("./sounds") if isfile(join("./sounds", f))]
 
 minTime = 15
 maxTime = 180
+soundspath = "./sounds/"
 
 
 class Kevin(commands.Cog):
+    #Global vars
     vClient = None
-    treadStarted = False
     thread = None
+    allSounds = None
+    treadStarted = False
 
     def __init__(self, bot):
         self.bot = bot
-        treadStarted = False
         print('__init__ Kevin')
         
-
+    # Commands
     @commands.command()
     async def kevin(self, ctx):
         print(self.treadStarted)
         if not self.treadStarted:
+            self.refreshsounds()
             self.treadStarted = True
             self.playSound()
+            await ctx.send("J'arrive !")
 
     @commands.command()
-    async def stop(self, ctx):
+    async def true_help(self, ctx):
+        await ctx.send("Alors : \n $help : Affiche ce mess'\n $kevin : J'viens vous tenir companie dans le voice chat\n $addsound : Ajoute + de bonheur dans mon soundboard. T'faut link un son en pièce jointe !\n $daronned : J'pense t'as pas besoin d'explication...")
+
+
+    @commands.command()
+    async def addsound(self, ctx):
+        message = ctx.message.content
+        if str(ctx.message.attachments) == "[]": # Checks if there is an attachment on the message
+            await ctx.send("J'aurais p't'etre b'soin que tu me mette en pièce jointe un son frr")
+            return
+        else: # If there is it gets the filename from message.attachments
+            split_v1 = str(ctx.message.attachments).split("filename='")[1]
+            filename = str(split_v1).split("' ")[0]
+            if filename.endswith(".mp3") or filename.endswith(".ogg"): # Checks if it is a .mp3 or ogg file
+                await ctx.message.attachments[0].save(fp="sounds/{}".format(filename)) # saves the file
+                await ctx.send("Ayé j'l'ai")
+                self.refreshsounds()
+            
+
+    @commands.command()
+    async def daronned(self, ctx):
         print('stop')
         """Stops and disconnects the bot from voice"""
-        self.thread.cancel()
+        if self.thread.is_alive():
+            self.thread.cancel()
+        await ctx.send("ça va j'ai compris :unamused:")
         await ctx.voice_client.disconnect()
 
     @kevin.before_invoke
@@ -46,32 +71,35 @@ class Kevin(commands.Cog):
             if ctx.author.voice:
                 self.vClient = await ctx.author.voice.channel.connect()
             else:
-                await ctx.send("You are not connected to a voice channel.")
+                await ctx.send("oooh connecte toiiiii.")
                 raise commands.CommandError("Author not connected to a voice channel.")
         elif ctx.voice_client.is_playing():
             ctx.voice_client.stop()
-            
+
+    # Utility 
     def playSound(self):
-        soundNumber = random.randrange(0, len(allSounds) - 1)
+        soundNumber = random.randrange(0, len(self.allSounds) - 1)
         print(f'played sound {soundNumber}')
         if self.vClient.is_playing():
             self.thread = threading.Timer(3, self.playSound).start()
             return
 
-        source = discord.FFmpegOpusAudio(f'sounds/{allSounds[soundNumber]}')
+        source = discord.FFmpegOpusAudio(f'sounds/{self.allSounds[soundNumber]}')
         self.vClient.play(source)
         if self.vClient.is_connected():
             delay = random.randrange(minTime, maxTime)
             print(f"Next sound in : {delay}")
             self.thread = threading.Timer(delay, self.playSound).start()
 
+    def refreshsounds(self):
+        self.allSounds = [f for f in listdir(soundspath) if isfile(join(soundspath, f))]
 
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(
-    command_prefix = commands.when_mentioned_or("!"),
+    command_prefix = commands.when_mentioned_or("$"),
     description = 'Typical kevin behavior',
     intents=intents,
 )
